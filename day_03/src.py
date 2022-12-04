866c7df64ecec4cdf8855a159f7219bf2619652f
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2022-12-04 10:00:42 +0000

    Day 4, part 1
    And some other bits

---
+++ day_03/src.py
@@ -0,0 +1,36 @@
+import string
+from typing import Sequence, Iterable
+
+from utils.read_in import read_in_from_file
+
+
+def priorities() -> dict[str, int]:
+    lower = {l: i+1 for i, l in enumerate(string.ascii_lowercase)}
+    upper = {l: i+27 for i, l in enumerate(string.ascii_uppercase)}
+    return {**lower, **upper}
+
+
+def find_shared(rucksack: str) -> str:
+    length = len(rucksack)
+    halfway = int(length/2)
+    first = rucksack[:halfway]
+    second = rucksack[halfway:]
+    return find_badge([first, second])
+
+
+def find_badge(rucksacks: Sequence[str]) -> str:
+    return set(rucksacks[0]).intersection(*map(set, rucksacks[1:])).pop()
+
+
+def part_one() -> int:
+    return sum(map(lambda c: priorities().get(c), map(find_shared, read_in_from_file("day_03/input.txt"))))
+
+
+def group_in_threes(rucksacks: Sequence[str]) -> Iterable[list[str]]:
+    for i, line in enumerate(rucksacks):
+        if i % 3 == 0:
+            yield rucksacks[i:i+3]
+
+
+def part_two() -> int:
+    return sum(map(lambda c: priorities()[c], map(find_badge, group_in_threes(read_in_from_file("day_03/input.txt")))))
---
+++ day_04/src.py
@@ -0,0 +1,31 @@
+import dataclasses
+from utils.read_in import read_in_from_file
+
+
+@dataclasses.dataclass
+class CleaningArea:
+    start: int
+    end: int
+
+    def contains(self, other_area: 'CleaningArea') -> bool:
+        return self.start <= other_area.start and self.end >= other_area.end
+
+
+def read_in_areas() -> list[tuple[CleaningArea, CleaningArea]]:
+    return [tuple(map(create_cleaning_area, line.split(","))) for line in read_in_from_file("day_04/input.txt")]  # type: ignore
+
+
+def create_cleaning_area(area_as_string: str) -> CleaningArea:
+    area = area_as_string.split("-")
+    return CleaningArea(int(area[0]), int(area[1]))
+
+
+def fully_contains(area_pair: tuple[CleaningArea, CleaningArea]) -> bool:
+    first_area, second_area = area_pair
+    return first_area.contains(second_area) or second_area.contains(first_area)
+
+
+def count_fully_contains() -> int:
+    area_pairs = read_in_areas()
+    return sum(map(fully_contains, area_pairs))
+
